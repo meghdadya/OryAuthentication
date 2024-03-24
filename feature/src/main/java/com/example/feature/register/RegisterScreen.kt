29b8089726cb25efb9bc5830e1d5243a7c0ff979
meghdad <meghdadya@gmail.com>
2024-03-24 23:52:24 +0330

    Little Ui Fix

--- feature/src/main/java/com/example/feature/register/RegisterScreen.kt
+++ feature/src/main/java/com/example/feature/register/RegisterScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -148,7 +149,7 @@ fun RegisterScreen(items: List<RegisterDomainModel>, onClicked: (String, String,
                         onClicked(
                             email, password, fullName
                         )
-                    }) {
+                    }, modifier = Modifier.width(200.dp)) {
                         Text(text = item.label)
                     }
                 }
